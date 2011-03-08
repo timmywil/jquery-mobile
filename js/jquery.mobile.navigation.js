timmywil/jquery-mobile

commit 20dce2ce4294c4036ed057902d7f02c3c3086b4a

small refactor to simplify newActiveIndex tracking

js/jquery.mobile.navigation.js
@@ -288,7 +288,7 @@
 		if( fromHashChange ){
 
 			// determine new page index
-			var newActiveIndex = null;
+			var newActiveIndex;
 
 			// check if url is in history and if it's ahead or behind current page
 			$.each( urlHistory.stack, function( i ){
@@ -304,7 +304,7 @@
 			});
 
 			// save new page index
-			urlHistory.activeIndex = ( newActiveIndex != null ? newActiveIndex : urlHistory.activeIndex );
+			urlHistory.activeIndex = newActiveIndex ? newActiveIndex : urlHistory.activeIndex;
 
 			//if it's a back, use reverse animation
 			if( back ){

commit c22fe3fae285bd4dc0a8387e588c599236f33d3b

Fix for issue 1207 - Can't revisit the same page after visitin it, hitting the back button and clicking its link
- This was a regression introduced by 20dce2ce. Added check for undefined newActiveIndex so we can tell the difference between the URL not being found, and a zero index.

js/jquery.mobile.navigation.js
@@ -305,7 +305,7 @@
 			});
 
 			// save new page index
-			urlHistory.activeIndex = newActiveIndex ? newActiveIndex : urlHistory.activeIndex;
+			urlHistory.activeIndex = newActiveIndex !== undefined ? newActiveIndex : urlHistory.activeIndex;
 
 			//if it's a back, use reverse animation
 			if( back ){